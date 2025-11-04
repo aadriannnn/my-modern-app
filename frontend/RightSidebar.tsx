ab9b73c52677ea0ff7cc5eaaa796728f2ae68298
Adrian Nicolau <nicolauadi3@gmail.com>
2025-11-04 14:37:53 +0200

    feat: actualizat frontend + backend treizeci si sase

---
+++ frontend/RightSidebar.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const RightSidebar = () => {
+  return (
+    <aside className="w-1/4 bg-gray-100 p-4">
+      {/* This is a placeholder for the right sidebar content. */}
+    </aside>
+  );
+};
+
+export default RightSidebar;
