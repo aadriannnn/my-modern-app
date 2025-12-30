06ffca10e9a7dd7ab23a3df9a3b663d53d921180
Adrian Nicolau <nicolauadi3@gmail.com>
2025-12-30 16:05:53 +0200

    feat: actualizat frontend + backend patrusutesapteoptzecisi doi

--- frontend/src/components/LegalNews/ArticleFeedItem.tsx
+++ frontend/src/components/LegalNews/ArticleFeedItem.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Calendar, Scale } from 'lucide-react';
+import { Scale } from 'lucide-react';
 
 interface Article {
     title: string;
